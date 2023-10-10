7af8fb89389ce3c187fb0cf6a4516aad792586e6
Fidan <parlakfidan1981@gmail.com>
2023-10-10 16:28:34 +0200

    Addet_TC: ONB2-365 On the New Resource page, verify that the "+ Add resource" button is visible and clickable.

---
+++ src/test/java/ch/mycomp/onboarding/step_definitions/ResourcesStepDefinition.java
@@ -0,0 +1,47 @@
+package ch.mycomp.onboarding.step_definitions;
+
+import ch.mycomp.onboarding.pages.LoginPage;
+import ch.mycomp.onboarding.pages.ResourcesPage;
+import ch.mycomp.onboarding.utilities.BrowserUtils;
+import ch.mycomp.onboarding.utilities.ConfigurationReader;
+import ch.mycomp.onboarding.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class ResourcesStepDefinition {
+    LoginPage loginPage = new LoginPage();
+    ResourcesPage resourcesPage=new ResourcesPage();
+    @Given("The user goes to the sign-in page")
+    public void theUserGoesToTheSignInPage() {
+        Driver.get().get(ConfigurationReader.get("myCompSignin"));
+    }
+
+    @When("The user enters valid logon credentials")
+    public void theUserEntersValidLogonCredentials() {
+        loginPage.login();
+    }
+
+
+    @Then("The user clicks on the {string} category")
+    public void theUserClicksOnTheCategory(String navigationName) {
+        WebElement sidebar = Driver.get().findElement(By.xpath("//span[text()='" + navigationName + "']"));
+        BrowserUtils.clickWithJS(sidebar);
+    }
+
+    @Then("The user clicks on the {string} button")
+    public void theUserClicksOnTheButton(String buttonName) {
+        BrowserUtils.waitFor(2);
+        WebElement button = Driver.get().findElement(By.xpath("//span[text()='" + buttonName + "']"));
+        BrowserUtils.clickWithJS(button);
+    }
+
+    @And("The user should be seen the new recources item opened")
+    public void theUserShouldBeSeenTheNewRecourcesItemOpened() {
+        Assert.assertTrue(resourcesPage.resourcesSite.isDisplayed());
+    }
+}
---
+++ src/test/resources/features/Resources.feature
@@ -0,0 +1,10 @@
+Feature: Resources
+Scenario:TC: ONB2-365 On the New Resource page, verify that the "+ Add resource" button is visible and clickable.
+
+  Given The user goes to the sign-in page
+  When The user enters valid logon credentials
+  Then The user clicks on the "Resources" category
+  Then The user clicks on the "New Resource" button
+  Then The user clicks on the "Add resource" button
+  And The user should be seen the new recources item opened
+
