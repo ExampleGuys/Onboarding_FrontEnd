abbc9ab9c7e7123b0a000a21cf22b972b9a93096
Fidan <parlakfidan1981@gmail.com>
2023-10-10 22:25:03 +0200

    Addet_TC: ONB2-365 On the New Resource page, verify that the "+ Add resource" button is visible and clickable.

--- src/test/java/ch/mycomp/onboarding/pages/ResourcesPage.java
+++ src/test/java/ch/mycomp/onboarding/pages/ResourcesPage.java
@@ -1,5 +1,6 @@
 package ch.mycomp.onboarding.pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -8,5 +9,23 @@ public class ResourcesPage extends BasePage{
     //Resources >resourcesSite
     @FindBy(xpath = "(//div[@class='ant-row css-14bavl3'])[1]")
     public WebElement resourcesSite;
+
+    //Resources >listOfResourcesTitleText
+    @FindBy(xpath = "//div[text()='List of Resources']")
+    public WebElement listOfResourcesTitleText;
+
+    //Resources > Category Name Title
+    @FindBy(xpath = "//label[@title='Category Name']")
+    public WebElement categoryNameTitle;
+
+    //Resources > List of Resources altindaki ilk sütun/searchBoxFirstLine
+    @FindBy(xpath = "(//tr[@class='ant-table-row ant-table-row-level-0'])[1]")
+    public WebElement searchBoxFirstLine;
+
+
+
+
+
+
 
 }
