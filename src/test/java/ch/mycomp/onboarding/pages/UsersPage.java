7dbcad61d73443a2433fc03b102177603b378fb9
AhmetTestTechno <ahmet.bicakci@testtechno.com>
2023-10-14 19:09:35 +0200

    Added TC ONB2 51 Verification of the Access to the Users page

--- src/test/java/ch/mycomp/onboarding/pages/UsersPage.java
+++ src/test/java/ch/mycomp/onboarding/pages/UsersPage.java
@@ -1,8 +1,11 @@
 package ch.mycomp.onboarding.pages;
 
+import ch.mycomp.onboarding.utilities.Driver;
 import com.github.javafaker.Faker;
 import com.github.javafaker.Faker;
 import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
@@ -10,7 +13,18 @@ import org.openqa.selenium.support.PageFactory;
 
 import java.net.PortUnreachableException;
 
+import static ch.mycomp.onboarding.utilities.Driver.driver;
+
 public class UsersPage extends BasePage{
+
+    JavascriptExecutor jse = (JavascriptExecutor) driver;
+
+
+
+
+
+
+    static Actions actions = new Actions(Driver.get());
 
 
 
@@ -25,7 +39,34 @@ public class UsersPage extends BasePage{
 
     public void assertionForlistofUsersIsVisible() {
         assert (listofUsers.isDisplayed());
-
-
     }
+
+
+    @FindBy(xpath = "//span[@class='ant-breadcrumb-link']")
+    public WebElement titleofUsers;
+
+
+    public void scrollDown(){
+
+        WebElement scrolldownpUsersPage= driver.
+                findElement(By.xpath("//*[@id=\"root\"]/section/section/main/div[2]/div[2]/div/div[2]/div/div/div/ul"));
+        actions.moveToElement(scrolldownpUsersPage).perform();
+    }
+
+
+    @FindBy(xpath = "//span[@class='ant-select-arrow']")
+    public WebElement usersPageArrow;
+
+
+    @FindBy(xpath = "/*[@id=\"root\"]/section/section/main/")
+    public WebElement tenUsers;
+
+
+    @FindBy(xpath = "//li[@class='ant-pagination-total-text']")
+    public WebElement totalNumberOfUsers;
+
+    @FindBy(xpath = "(//a[@rel='nofollow'])[1]")
+    public WebElement theNumberofUsers;
+
+
 }
