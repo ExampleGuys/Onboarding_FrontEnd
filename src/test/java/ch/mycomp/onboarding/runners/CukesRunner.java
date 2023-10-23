05ce4eaaf6176c7ea45c424decf92936a14cc472
Yavuz Selim Hasturk <yavuzselimhasturk@gmail.com>
2023-10-23 14:46:11 +0300

    Added TC ONB2-302 The mini-calendar should be available in the first start date section

--- src/test/java/ch/mycomp/onboarding/runners/CukesRunner.java
+++ src/test/java/ch/mycomp/onboarding/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "ch/mycomp/onboarding/step_definitions",
         dryRun = false,
-        tags = "@regression"
+        tags = ""
 )
 public class CukesRunner {
 }
