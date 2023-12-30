57a7556f16fee021502480a9fc2e798c7335a358
Yavuz Selim Hasturk <yavuzselimhasturk@gmail.com>
2023-12-30 18:08:48 +0300

    ADded ONMYC-982

--- src/test/java/ch/mycomp/onboarding/pages/SitesPage.java
+++ src/test/java/ch/mycomp/onboarding/pages/SitesPage.java
@@ -3,4 +3,27 @@ package ch.mycomp.onboarding.pages;
 
 public class SitesPage extends BasePage {
 
+    public void theUserEnterSiteNameInInTheField(String boxName) {
+        boxName(boxName).sendKeys(faker.address().cityName());
+    }
+
+    public void theUserEnterCountryNameInTheField(String boxName) {
+        boxName(boxName).sendKeys(faker.country().name());
+    }
+
+    public void theUserEnterCityNameInTheField(String boxName) {
+        boxName(boxName).sendKeys(faker.address().cityName());
+    }
+
+    public void theUserEnterZipCodeInTheField(String boxName) {
+        boxName(boxName).sendKeys(faker.address().zipCode());
+    }
+
+    public void theUserEnterStreetNameInTheField(String boxName) {
+        boxName(boxName).sendKeys(faker.address().streetName());
+    }
+
+    public void theUserEnterStreetNumberInTheField(String boxName) {
+        boxName(boxName).sendKeys(faker.address().streetAddressNumber());
+    }
 }
--- src/test/java/ch/mycomp/onboarding/step_definitions/SitesStepDefs.java
+++ src/test/java/ch/mycomp/onboarding/step_definitions/SitesStepDefs.java
@@ -1,6 +1,36 @@
 package ch.mycomp.onboarding.step_definitions;
 
 
-public class SitesStepDefs extends ObjectIndex {
+import io.cucumber.java.en.And;
 
+public class SitesStepDefs extends ObjectIndex {
+    @And("The user enter site name in in the {string} field")
+    public void theUserEnterSiteNameInInTheField(String boxName) {
+           sitesPage.theUserEnterSiteNameInInTheField(boxName);
+    }
+
+        @And("The user enter country name in the {string} field")
+        public void theUserEnterCountryNameInTheField(String boxName) {
+            sitesPage.theUserEnterCountryNameInTheField(boxName);
         }
+
+        @And("The user enter city name in the {string} field")
+        public void theUserEnterCityNameInTheField(String boxName) {
+        sitesPage.theUserEnterCityNameInTheField(boxName);
+        }
+
+    @And("The user enter zip code in the {string} field")
+    public void theUserEnterZipCodeInTheField(String boxName) {
+        sitesPage.theUserEnterZipCodeInTheField(boxName);
+    }
+
+    @And("The user enter street name in the {string} field")
+    public void theUserEnterStreetNameInTheField(String boxName) {
+        sitesPage.theUserEnterStreetNameInTheField(boxName);
+    }
+
+    @And("The user enter street number in the {string} field")
+    public void theUserEnterStreetNumberInTheField(String boxName) {
+        sitesPage.theUserEnterStreetNumberInTheField(boxName);
+    }
+}
